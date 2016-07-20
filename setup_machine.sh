824d646357b0caac069cbde7fdbc52568b94d628
Armen Zambrano G <armenzg@mozilla.com>
2016-07-20 14:48:11 -0400

    Remove old stuff

--- setup_machine.sh
+++ setup_machine.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 set -ex
 DIR=`pwd`
-CONFIG_FILES=`cd config_files && find . -type f | sed 's|./||'`
-# Create the config giles
+CONFIG_FILES=`cd dotfiles/linux && find . -type f | sed 's|./||'`
+# Create the config files
 cd ~
 # XXX: we have to verify that there is a file to remove; otherwise it will fail
 for file in $CONFIG_FILES; do rm ~/$file && ln -s $DIR/config_files/$file .; done
-# Create the scripts
-mkdir -p ~/moz/scripts
-cp $DIR/scripts/* ~/moz/scripts
